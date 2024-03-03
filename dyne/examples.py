0db57231b73e19c113cb21724d059ef524b9e4c1
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2024-03-04 03:04:20 +0500

    TST: Add result equivalence of all algorithms with linear filter/smoother on a linear problem

--- dyne/examples.py
+++ dyne/examples.py
@@ -54,6 +54,71 @@ def generate_linear_pendulum(
     return x0, P0, xt, wt, F, G, Q, n_epoch, [(np.arange(n_epoch), z, H, R)]
 
 
+def generate_linear_pendulum_as_nl_problem(
+    n_epoch=1000,
+    x0=np.array([1.0, 0.0]),
+    P0=np.diag([0.1**2, 0.05**2]),
+    tau=0.1,
+    T=10.0,
+    eta=0.1,
+    qf=0.03,
+    sigma_angle=0.2,
+    sigma_rate=0.1,
+    rng=0,
+):
+    """Prepare data for an example of linear pendulum with friction.
+
+    The continuous system model is::
+
+        dx1 / dt = x2
+        dx2 / dt = -omega**2 * x1 - 2 * eta * omega * x2 + f
+
+    with ``f`` being an external force. It is discretized with a time step `tau`,
+    the external force is modeled as a random white sequence.
+
+    The measurements consist of both x1 and x2 (angle and angular rate).
+
+    This function returns the problem as a general nonlinear problem which can be
+    used for testing and verification purposes.
+    """
+    rng = check_random_state(rng)
+    n_states = 2
+    n_noises = 1
+    n_obs = 2
+
+    xt = np.empty((n_epoch, n_states))
+    xt[0] = rng.multivariate_normal(x0, P0)
+
+    omega = 2 * np.pi / T
+    F = np.array([[1, tau], [-(omega ** 2) * tau, 1 - 2 * eta * omega * tau]])
+    G = np.array([[0], [1]])
+    Q = np.array([[tau * qf**2]])
+    wt = np.empty((n_epoch - 1, n_noises))
+
+    R = np.diag([sigma_angle**2, sigma_rate**2])
+    H = np.identity(n_obs)
+    z = []
+
+    def f(k, X, W=None, with_jacobian=True):
+        if W is None:
+            W = np.zeros(1)
+        X_next = F @ X + G @ W
+        if not with_jacobian:
+            return X_next
+        return X_next, F, G
+
+    def h(k, X, with_jacobian=True):
+        return (H @ X, H) if with_jacobian else H @ X
+
+    for i in range(n_epoch):
+        z.append(H @ xt[i] + rng.multivariate_normal(np.zeros(n_obs), R))
+        if i + 1 < n_epoch:
+            wt[i] = rng.multivariate_normal(np.zeros(len(Q)), Q)
+            xt[i + 1] = F @ xt[i] + G @ wt[i]
+
+    return x0, P0, xt, wt, f, Q, n_epoch, [(np.arange(n_epoch), z, h, R)]
+
+
 def generate_nonlinear_pendulum(
     n_epochs=1000,
     X0=np.array([0.5 * np.pi, 0]),
--- dyne/tests/test_algorithms_basic.py
+++ dyne/tests/test_algorithms_basic.py
@@ -65,3 +65,21 @@ def test_mhf():
         en = (result.X - Xt) / np.diagonal(result.P, axis1=1, axis2=2) ** 0.5
         assert np.all(dyne.util.compute_rms(en) > 0.7)
         assert np.all(dyne.util.compute_rms(en) < 1.3)
+
+
+def test_linear_equivalence():
+    X0, P0, Xt, Wt, F, G, Q, n_epochs, measurements_lin = (
+        dyne.examples.generate_linear_pendulum())
+    *_, f, _, _, measurements = dyne.examples.generate_linear_pendulum_as_nl_problem()
+
+    kf_result = dyne.run_kalman_smoother(X0, P0, F, G, Q, n_epochs, measurements_lin)
+
+    for algorithm, options in zip([dyne.run_ekf, dyne.run_ukf, dyne.run_mhf],
+                                  [{}, {}, dict(window=1)]):
+        result = algorithm(X0, P0, f, Q, n_epochs, measurements, **options)
+        difference = result.X - kf_result.xf
+        assert np.all(np.abs(difference) < 1e-15)
+
+    result_opt = dyne.run_optimization(X0, P0, f, Q, n_epochs, measurements)
+    difference = result_opt.X - kf_result.x
+    assert np.all(np.abs(difference) < 1e-15)
